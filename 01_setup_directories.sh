ba6a016c4d318a06358fa668156671a30128a5f2
Joe Manthey <jdmanthey@gmail.com>
2025-07-03 16:55:05 -0500

    Create 01_setup_directories.sh

---
+++ 01_setup_directories.sh
@@ -0,0 +1,55 @@
+mkdir 00_fastq_h
+mkdir 00_fastq_m
+mkdir 01_cleaned_h
+mkdir 01_cleaned_m
+mkdir 01_split_h
+mkdir 01_working_h
+mkdir 01_bam_files
+mkdir 01b_bam_files
+mkdir 02_vcf
+mkdir 02b_vcf
+mkdir 03_vcf
+mkdir 03b_vcf
+mkdir 04_vcf
+mkdir 04b_vcf
+mkdir 05_pca
+mkdir 05b_pca
+
+mkdir 06_stats_atl
+mkdir 06_stats_atl/windows
+mkdir 06b_stats_atl
+mkdir 06b_stats_atl/windows
+
+mkdir 06_stats_car
+mkdir 06_stats_car/windows
+mkdir 06b_stats_car
+mkdir 06b_stats_car/windows
+
+mkdir 06_stats_cat
+mkdir 06_stats_cat/windows
+mkdir 06b_stats_cat
+mkdir 06b_stats_cat/windows
+
+mkdir 06_stats_myi
+mkdir 06_stats_myi/windows
+mkdir 06b_stats_myi
+mkdir 06b_stats_myi/windows
+
+mkdir 07_transversions
+mkdir 07b_transversions
+
+mkdir 20_combine_lanes
+mkdir 21_split_h_script
+mkdir 22_dedup_h_script
+mkdir 23_cat_h_script
+mkdir 24_dedup2_h_script
+mkdir 25_merge_h_script
+mkdir 26_qual_h_script
+mkdir 27_align_script_h
+mkdir 28_stats_script_h
+mkdir 29_cat_modern_script
+mkdir 30_align_modern_script
+mkdir 31_downsample_script
+mkdir 32_genotype_script
+mkdir 33_merge_filter_script
+mkdir 34_genotyping_stats
